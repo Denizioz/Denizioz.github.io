ac705f59fcac96b7682da000449ebf28896852e0
Denizioz <denizioz@hotmail.com>
2021-02-09 00:08:04 +0100

    For production

---
+++ dist/precache-manifest.604e35a9776f43628a2fda6f7db7c1a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d72fe0ec58b4d08609",
+    "url": "/css/app.4c7ef6ce.css"
+  },
+  {
+    "revision": "74649c84cd5c1a33ddee",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "de693e97a0579db80b9470db052d5d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32d72fe0ec58b4d08609",
+    "url": "/js/app.39c2352b.js"
+  },
+  {
+    "revision": "74649c84cd5c1a33ddee",
+    "url": "/js/chunk-vendors.03954f67.js"
+  },
+  {
+    "revision": "1595b7b46b12d85a0f92bb2ce878efe4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
